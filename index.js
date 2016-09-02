bb642a9f00ddbad3c7a38a730b3709f915fb8f36
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-09-02 14:37:27 -0400

    got sockets connected

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var mongoose = require("mongoose")
 var express = require("express")
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', function(socket){
+  console.log("im in!");
+})
 
-app.listen(3000, function(){
+http.listen(3000, function(){
   console.log("listening on 3000")
 })
