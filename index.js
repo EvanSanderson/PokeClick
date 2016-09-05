02736fa4a96d57994d3d18ba69e2acb70af8257d
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-09-05 14:09:36 -0400

    adding mongod

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-var mongoose = require("mongoose")
-var express = require("express")
-
+var mongoose = require("mongoose");
+var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+mongoose.connect("mongodb://localhost/pokeclick");
+var Click = mongoose.model("Click", new mongoose.Schema({
+  click: Number
+}));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get("/api/clicks", function(req, res){
+  Click.find({}).lean().then(function(clicks){
+    res.json(clicks)
+  })
+})
+
 io.on('connection', function(socket){
   socket.on('clicked', function(click){
-    io.emit('clicked', click)
+    io.emit('clicked', click);
+    if(click) Click.create({click:click})
     console.log(click)
   })
 })
